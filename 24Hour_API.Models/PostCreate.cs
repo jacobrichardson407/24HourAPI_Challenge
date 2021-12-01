b8a73fb4222d0865751ba30256b131f0502460b8
Spencer Marsh <scmarsh421@gmail.com>
2021-11-30 21:14:40 -0500

    Post Create

--- 24Hour_API.Models/PostCreate.cs
+++ 24Hour_API.Models/PostCreate.cs
@@ -8,5 +8,12 @@ namespace _24Hour_API.Models
 {
     public class PostCreate
     {
+        [Required]
+        [Minlength(2, ErrorMessage = "Please enter at least 2 characters.")]
+        [MaxLength(100, ErrorMessage = "There are too many characters in this field.")]
+        public string Title { get; set; }
+        [MaxLenght(8000)]
+        public string Text { get; set; }
+        public int PostId { get; set; }
     }
 }
