97c54c6a1cf71b5ec1b088bdf9a8ad165241ce19
Jacob Richardson <jacobrichardson407@gmail.com>
2021-11-30 21:17:02 -0500

    commit ????

--- 24Hour_API.Models/PostCreate.cs
+++ 24Hour_API.Models/PostCreate.cs
@@ -9,11 +9,5 @@ namespace _24Hour_API.Models
 {
     public class PostCreate
     {
-        [Required]
-        [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
-        [MaxLength(100, ErrorMessage = "There are too many characters in this field.")]
-        public string Title { get; set; }
-        [MaxLength(8000)]
-        public string Content { get; set; }
     }
 }
